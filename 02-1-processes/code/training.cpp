8b90adaf6764b57045ab4e8b41a27c1606f15832
FranciscoCardita <franciscocardita@ua.pt>
2023-09-21 10:55:48 +0100

    Add training exercise

---
+++ 02-1-processes/code/training.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include "delays.h"
+#include "process.h"
+
+int main(void)
+{
+    pid_t ret =  pfork();
+
+    for (u_int8_t i = 1; i <= 20; i++)
+    {
+        if (ret == 0 && i <= 10)
+            printf("I'm the child: %d\n", i);
+        else if (ret != 0 && i > 10)
+        {
+            usleep(2000);
+            printf("I'm the parent: %d\n", i);
+        }            
+    }    
+}
