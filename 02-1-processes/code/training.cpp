50202cd7c14bf52e05e8583f0bdf796d7874e6cc
FranciscoCardita <franciscocardita@ua.pt>
2023-09-22 17:48:08 +0100

    Ex 1 refactor

--- 02-1-processes/code/training.cpp
+++ 02-1-processes/code/training.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 
 #include "delays.h"
@@ -11,14 +10,14 @@ int main(void)
 {
     pid_t ret =  pfork();
 
-    for (u_int32_t i = 1; i <= 20; i++)
+    if (ret == 0)
     {
-        if (ret == 0 && i <= 10)
+        for (int i = 1; i <= 10; i++)
             printf("I'm the child: %d\n", i);
-        else if (ret != 0 && i > 10)
-        {
-            usleep(2000);
+    } else
+    {
+        pwait(NULL);
+        for (int i = 11; i <= 20; i++)
             printf("I'm the parent: %d\n", i);
-        }            
-    }    
+    }  
 }
