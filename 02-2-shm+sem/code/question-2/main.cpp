584071d2134c142f363536861d7540872a7fd9ec
FranciscoCardita <franciscocardita@ua.pt>
2023-09-28 10:52:27 +0100

    Add question-2

---
+++ 02-2-shm+sem/code/question-2/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include "process.h"
+
+int main(void)
+{
+    
+}
